6836aa9c51d7a0ab5ee194c67813c46851064f66
Bruno Castro <brunohcastro@hotmail.com>
2021-11-18 03:21:54 -0300

    fix: move type overrides to index.ts

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -3,3 +3,15 @@ export * from './middlewares';
 export * from './validation';
 export * from './handler';
 export * from './HttpStatus';
+
+declare namespace NarsilExpress {
+  export type RequestContainer = import('awilix').AwilixContainer;
+}
+
+declare module 'http' {
+  export interface IncomingMessage {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    id: string | number | object;
+    container: NarsilExpress.RequestContainer;
+  }
+}
